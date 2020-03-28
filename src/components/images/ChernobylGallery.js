1e7779fd90c19586cdae97997e1b37241150fb8b
Bronson Avila <bronson.avila@gmail.com>
2020-03-28 12:23:01 -1000

    Remove commented-out code

--- src/components/images/ChernobylGallery.js
+++ src/components/images/ChernobylGallery.js
@@ -12,22 +12,7 @@ import Img from "gatsby-image";
  * - `gatsby-image`: https://gatsby.dev/gatsby-image
  * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
  */
-
 const ChernobylGallery = () => {
-  // const data = useStaticQuery(graphql`
-  //   query {
-  //     placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
-  //       childImageSharp {
-  //         fluid(maxWidth: 300) {
-  //           ...GatsbyImageSharpFluid
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
-
-  // return <Img fluid={data.placeholderImage.childImageSharp.fluid} />;
-
   const data = useStaticQuery(graphql`
     query {
       allFile(
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from "react";
 import { Link } from "gatsby";
 
-// import Image from "../components/Image";
 import SEO from "../components/SEO";
-// import AllFileTable from "../components/AllFileTable";
 import MarkdownList from "../components/MarkdownList";
 
 import ChernobylGallery from "../components/images/ChernobylGallery";
@@ -16,10 +14,6 @@ const IndexPage = () => (
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
     <ChernobylGallery />
-    {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div> */}
-    {/* <AllFileTable /> */}
     <MarkdownList />
     <Link to="/page-2/">Go to page 2</Link>
   </>
