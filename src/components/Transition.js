9943c3d94351c7718ec3a5fec86c29db9fc4b150
Bronson Avila <bronson.avila@gmail.com>
2020-03-28 18:52:07 -1000

    Modify Transition delay

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -4,13 +4,14 @@ const duration = 350;
 
 const Transition = posed.div({
   enter: {
-    delay: duration / 4,
+    delay: duration / 2,
     filter: 'blur(0px)',
     opacity: 1,
     y: 0,
     transition: { duration }
   },
   exit: {
+    delay: duration / 2,
     filter: 'blur(10px)',
     opacity: 0,
     y: 50,
