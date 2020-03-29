ad69218e758bca43979036a1984874c646040f55
Bronson Avila <bronson.avila@gmail.com>
2020-03-29 12:49:22 -1000

    Remove blur filter (re: Firefox performance)

--- src/components/Transition.js
+++ src/components/Transition.js
@@ -5,14 +5,12 @@ const delay = 350;
 const Transition = posed.div({
   enter: {
     delay: delay / 2,
-    filter: 'blur(0px)',
     opacity: 1,
     y: 0,
     transition: { duration: delay },
   },
   exit: {
     delay: delay / 2,
-    filter: 'blur(10px)',
     opacity: 0,
     y: 50,
     transition: { duration: delay / 2 },
