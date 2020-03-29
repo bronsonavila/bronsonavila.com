f9caac05db5635b3a666bdcbd972f1e44abd2055
Bronson Avila <bronson.avila@gmail.com>
2020-03-29 12:07:13 -1000

    Configure gallery lazy loading

--- src/templates/galleries.js
+++ src/templates/galleries.js
@@ -1,21 +1,53 @@
 import React, { useEffect } from 'react';
 import { graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import Img from 'gatsby-image/withIEPolyfill';
+
+import { lazyLoad } from '../utils/lazyLoad';
+
+const delay = 300; // For lazy loading animation.
+
+/**
+ * Changes the display of `.gallery__card` elements from `none` to `block` at
+ * staggered intervals. This allows the first batch of initially displayed
+ * cards to be revealed in sequential order rather than all at once. This is
+ * because the `lazyLoad` IntersectionObserver will not detect elements until
+ * `display: none` has been changed to a visible value.
+ */
+const displayGalleryCards = () => {
+  const cards = [...document.querySelectorAll('.gallery__card')];
+
+  cards.forEach((card, index) => {
+    setTimeout(() => {
+      card.style.display = 'block';
+    }, (index * delay) / 3.666);
+  });
+};
+
+/**
+ * Callback for the `lazyLoad` IntersectionObserver. Animates the entrance of
+ * each `.gallery__card` element.
+ *
+ * @param {Array} entries - An array of IntersectionObserverEntry objects.
+ */
+const observerCallback = entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      setTimeout(() => {
+        entry.target.style.height = '275px';
+        entry.target.style.opacity = 1;
+        entry.target.style.transform = 'scale(1) translateY(0)';
+      }, delay / 3);
+    }
+  });
+};
 
 export default ({ data }) => {
   const content = data.markdownRemark;
 
+  // Handle lazy loading:
   useEffect(() => {
-    const images = [...document.querySelectorAll('.gatsby-image-wrapper')];
-    images.forEach((image, index) => {
-      setTimeout(() => {
-        image.style.display = 'block';
-      }, (index * 300) / 3.666);
-      setTimeout(() => {
-        image.style.opacity = 1;
-        image.style.transform = 'scale(0.9) translateY(0)';
-      }, (index * 300) / 3);
-    });
+    displayGalleryCards();
+    lazyLoad(observerCallback);
   });
 
   return (
@@ -24,21 +56,25 @@ export default ({ data }) => {
         <h1>{content.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: content.html }} />
       </div>
-      <div className="flex flex-wrap">
+      <div className="gallery__card-container">
         {data.allFile.edges.map((image, index) => (
-          <Img
-            alt={image.node.base.split('.')[0]}
-            className="w-1/3 transition duration-300 ease-in-out"
-            fluid={image.node.childImageSharp.fluid}
-            data-element="image"
+          <div
+            className="gallery__card observable"
             key={index}
             style={{
               display: 'none',
-              maxHeight: '184.16px',
+              height: '0',
               opacity: 0,
-              transform: `scale(0.8) translateY(100px)`,
+              transform: 'scale(0.9) translateY(100px)',
+              willChange: 'transform',
             }}
-          />
+          >
+            <Img
+              alt={image.node.base.split('.')[0]}
+              className="h-full w-full"
+              fluid={image.node.childImageSharp.fluid}
+            />
+          </div>
         ))}
       </div>
     </section>
@@ -57,14 +93,14 @@ export const query = graphql`
       filter: {
         sourceInstanceName: { eq: "images" }
         relativeDirectory: { eq: $relativeDirectory }
-      },
-      sort: {fields: name}
+      }
+      sort: { fields: name }
     ) {
       edges {
         node {
           base
           childImageSharp {
-            fluid(maxWidth: 300) {
+            fluid {
               ...GatsbyImageSharpFluid
             }
           }
