cc334282e88d9462df03033a4cfaa37755a1031d
Bronson Avila <bronson.avila@gmail.com>
2020-03-29 00:20:59 -1000

    Fix posts className

--- src/templates/posts.js
+++ src/templates/posts.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { graphql } from "gatsby";
+import React from 'react';
+import { graphql } from 'gatsby';
 
 export default ({ data }) => {
   const content = data.markdownRemark;
 
   return (
-    <article class="post">
+    <article className="post">
       <h1>{content.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: content.html }} />
     </article>
