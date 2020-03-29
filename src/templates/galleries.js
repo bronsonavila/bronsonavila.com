2343f0029734fb732db28dab5875ab363180c370
Bronson Avila <bronson.avila@gmail.com>
2020-03-29 00:20:44 -1000

    Add galleries animation

--- src/templates/galleries.js
+++ src/templates/galleries.js
@@ -1,9 +1,22 @@
-import React from "react";
-import { graphql } from "gatsby";
-import Img from "gatsby-image";
+import React, { useEffect } from 'react';
+import { graphql } from 'gatsby';
+import Img from 'gatsby-image';
 
 export default ({ data }) => {
   const content = data.markdownRemark;
+
+  useEffect(() => {
+    const images = [...document.querySelectorAll('.gatsby-image-wrapper')];
+    images.forEach((image, index) => {
+      setTimeout(() => {
+        image.style.display = 'block';
+      }, (index * 300) / 2.333);
+      setTimeout(() => {
+        image.style.opacity = 1;
+        image.style.transform = 'scale(0.9) translateY(0)';
+      }, (index * 300) / 2);
+    });
+  });
 
   return (
     <section className="gallery">
@@ -14,10 +27,17 @@ export default ({ data }) => {
       <div className="flex flex-wrap">
         {data.allFile.edges.map((image, index) => (
           <Img
-            alt={image.node.base.split(".")[0]}
-            className="w-1/3"
+            alt={image.node.base.split('.')[0]}
+            className="w-1/3 transition duration-300 ease-in-out"
             fluid={image.node.childImageSharp.fluid}
+            data-element="image"
             key={index}
+            style={{
+              display: 'none',
+              maxHeight: '184.16px',
+              opacity: 0,
+              transform: `scale(0.8) translateY(100px)`,
+            }}
           />
         ))}
       </div>
@@ -35,7 +55,7 @@ export const query = graphql`
     }
     allFile(
       filter: {
-        extension: { regex: "/(jpg)|(png)|(jpeg)/" }
+        sourceInstanceName: { eq: "images" }
         relativeDirectory: { eq: $relativeDirectory }
       }
     ) {
