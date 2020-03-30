d3fc6e3680b5fd17a5ef5c06a402ed8f9318e56a
Bronson Avila <bronson.avila@gmail.com>
2020-03-29 22:25:06 -1000

    Refactor Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Link } from 'gatsby';
+
+/**
+ * Reveals the footer when the page is no longer scrollable.
+ *
+ * @param {object} footerRef - The footer's `ref` object.
+ */
+const toggleActiveFooterState = footerRef => {
+  if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
+    if (footerRef && footerRef.current) {
+      footerRef.current.classList.add('is-active');
+    }
+  }
+};
+
+const Footer = () => {
+  const [isTicking, setIsTicking] = useState(false);
+  const footerRef = useRef(null);
+
+  useEffect(() => {
+    setTimeout(() => toggleActiveFooterState(footerRef), 800);
+
+    // Throttle scroll event. See:
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document/scroll_event
+    const handleScroll = () => {
+      if (!isTicking) {
+        window.requestAnimationFrame(() => {
+          toggleActiveFooterState(footerRef);
+          setIsTicking(false);
+        });
+        setIsTicking(true);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  });
+
+  return (
+    <div className="overflow-hidden">
+      <footer ref={footerRef}>
+        <div className="container h-12 mx-auto px-4 py-4">
+          <Link to="/" className="text-white no-underline">
+            © {new Date().getFullYear()} Bronson Avila
+          </Link>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
--- src/components/layout.js
+++ src/components/layout.js
@@ -4,58 +4,19 @@
  *
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
 
-const toggleActiveFooterState = footerRef => {
-  if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
-    if (footerRef && footerRef.current) {
-      footerRef.current.classList.add('is-active');
-    }
-  }
-};
+import Footer from './Footer';
 
-const Layout = ({ children }) => {
-  const [isTicking, setIsTicking] = useState(false);
-  const footerRef = useRef(null);
-
-  useEffect(() => {
-    setTimeout(() => toggleActiveFooterState(footerRef), 800);
-
-    const handleScroll = () => {
-      if (!isTicking) {
-        window.requestAnimationFrame(() => {
-          toggleActiveFooterState(footerRef);
-          setIsTicking(false);
-        });
-        setIsTicking(true);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
-  });
-
-  return (
-    <>
-      <div className="container h-full mx-auto px-4 py-6">
-        <main className="mb-16">{children}</main>
-      </div>
-      <div className="overflow-hidden">
-        <footer ref={footerRef}>
-          <div className="container h-12 mx-auto px-4 py-4">
-            <span className="text-white">
-              <Link to="/" className="text-white no-underline">
-                © {new Date().getFullYear()} Bronson Avila
-              </Link>
-            </span>
-          </div>
-        </footer>
-      </div>
-    </>
-  );
-};
+const Layout = ({ children }) => (
+  <>
+    <div className="container h-full mx-auto px-4 py-6">
+      <main className="mb-16">{children}</main>
+    </div>
+    <Footer />
+  </>
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
