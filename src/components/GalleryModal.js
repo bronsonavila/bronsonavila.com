6eed6a15a53b610c3f425c421a820eb1332d2898
Bronson Avila <bronson.avila@gmail.com>
2020-04-11 18:37:01 -1000

    Fix Gatsby build error re: undefined body

--- src/components/GalleryModal.js
+++ src/components/GalleryModal.js
@@ -3,8 +3,17 @@ import Img from 'gatsby-image/withIEPolyfill';
 
 const GalleryModal = React.forwardRef(
   ({ activeCardIndex, cardSize, height, images, width }, ref) => {
-    const initialX = document.body.scrollWidth * 1.01;
-    const initialY = document.body.scrollHeight * 1.01;
+    // `body` workaround required because Gatsby does not have `document` defined when
+    // building production files. See: https://github.com/gatsbyjs/gatsby/issues/309
+    const body = typeof document !== 'undefined' && document && document.body;
+    let initialX = 9999;
+    let initialY = 9999;
+
+    if (body) {
+      // Move modal just outside the `body` boundaries for a slightly quicker entrance.
+      initialX = body.scrollWidth * 1.01;
+      initialY = body.scrollHeight * 1.01;
+    }
 
     return (
       <div
