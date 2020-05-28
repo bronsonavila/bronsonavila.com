6f3ab25efc6a8301474b4aed3d6fbad81278d203
Bronson Avila <bronson.avila@gmail.com>
2020-05-28 11:32:05 -1000

    Delete test Image & MarkdownList components

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import SEO from '../components/SEO';
-import MarkdownList from '../components/MarkdownList';
 
 const IndexPage = () => (
   <div className="container mx-auto px-4">
     <SEO title="Home" />
     <h1 className="text-center pt-8 pb-1">Hello</h1>
-    <MarkdownList />
     <Link to="/photos/">Go to photos</Link>
   </div>
 );
