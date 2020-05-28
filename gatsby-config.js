084c8ebcfc676818271632672ad1ca5b04d1d004
Bronson Avila <bronson.avila@gmail.com>
2020-05-28 12:27:15 -1000

    Modify favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `B.A.`,
+    title: `Bronson Avila`,
     description: `Portfolio website.`,
     author: `@bronsonavila`,
   },
@@ -61,7 +61,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/logo.png`, // This path is relative to the root of the site.
       },
     },
     {
