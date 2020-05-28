068f9f5b27002db2ec4784f189a5b9853894d410
Bronson Avila <bronson.avila@gmail.com>
2020-05-28 12:27:34 -1000

    Add header /photos/ link

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 import Logo from './Logo';
 
 const Header = () => (
   <header>
     <div className="border-b border-gray-400 h-full mx-6">
-      <div className="container flex items-center h-full mx-auto px-4">
+      <div className="container flex items-center justify-between h-full mx-auto px-4">
         <Logo />
+        <div className="flex items-center">
+          <Link className="text-gray-600 hover:text-gray-900" to="/photos/">Photos</Link>
+        </div>
       </div>
     </div>
   </header>
