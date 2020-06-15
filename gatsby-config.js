e41e299958fea653368ce07a51490cfd242c720f
Bronson Avila <bronson.avila@gmail.com>
2020-06-14 15:25:31 -1000

    Remove `images` from `gatsby-source-filesystem`

--- gatsby-config.js
+++ gatsby-config.js
@@ -53,13 +53,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         name: `pages`,
         path: `${__dirname}/src/pages`,
       },
