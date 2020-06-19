6daf014b9ff412897dab3043e95bab47bd6f3d92
Bronson Avila <bronson.avila@gmail.com>
2020-06-18 18:30:03 -1000

    Clean up config

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,6 @@ module.exports = {
         anonymize: true,
         respectDNT: true,
         pageTransitionDelay: 850,
-        // Defers execution of google analytics script after page load
         defer: false,
       },
     },
@@ -43,12 +42,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-purgecss`,
       options: {
-        // printRejected: true, // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        tailwind: true, // Enable tailwindcss support
+        tailwind: true,
         whitelistPatterns: [/img/, /gatsby/],
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
       },
     },
     `gatsby-plugin-react-helmet`,
@@ -80,7 +75,7 @@ module.exports = {
         background_color: `#fafafa`,
         theme_color: `#a90000`,
         display: `minimal-ui`,
-        icon: `static/logo.png`, // This path is relative to the root of the site.
+        icon: `static/logo.png`,
       },
     },
     {
